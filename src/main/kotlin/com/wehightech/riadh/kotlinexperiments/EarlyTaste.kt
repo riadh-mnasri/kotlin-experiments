a8c9b250b77a71590118a6d045fcb50f46499825
Riadh MNASRI <riadh.mnasri@gmail.com>
2018-06-04 12:25:20 +0200

    Update oldest function

--- src/main/kotlin/com/wehightech/riadh/kotlinexperiments/EarlyTaste.kt
+++ src/main/kotlin/com/wehightech/riadh/kotlinexperiments/EarlyTaste.kt
@@ -4,8 +4,14 @@ data class Person(val name: String,
                   val age: Int? = null)
 
 fun main(args: Array<String>) {
-    val persons = listOf(Person("Mnasri"),
-        Person("Riadh", age = 35))
+    oldest()
+}
+
+fun oldest() {
+    val persons = listOf(Person("Mnasri", 35),
+        Person("Bouguila"))
     val oldest = persons.maxBy { it.age ?: 0 }
     println("The oldest is: $oldest")
 }
+
+
