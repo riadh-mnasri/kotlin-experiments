ed1e6800762770d9ba5d14ceb1ddb04c3d19570d
Riadh MNASRI <riadh.mnasri@gmail.com>
2018-07-29 13:33:38 +0200

    Define POM Main class

--- src/main/kotlin/com/wehightech/riadh/kotlinexperiments/MnemonicColor.kt
+++ src/main/kotlin/com/wehightech/riadh/kotlinexperiments/MnemonicColor.kt
@@ -3,6 +3,7 @@ package com.wehightech.riadh.kotlinexperiments
 fun main(args: Array<String>) {
 
     println(getMnemonic(Color.BLUE))
+    mix(Color.RED, Color.YELLOW)
 }
 
 enum class Color(
@@ -23,4 +24,12 @@ fun getMnemonic(color: Color) =
         Color.BLUE -> "Battle"
         Color.INDIGO -> "In"
         Color.VIOLET -> "Vain"
+    }
+
+fun mix(c1: Color, c2: Color) =
+    when (setOf(c1, c2)) {
+        setOf(Color.RED, Color.YELLOW) -> Color.ORANGE
+        setOf(Color.YELLOW, Color.BLUE) -> Color.GREEN
+        setOf(Color.BLUE, Color.VIOLET) -> Color.INDIGO
+        else -> throw Exception("Dirty color")
     }
